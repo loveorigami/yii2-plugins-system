17faaa91986a0f64e6f8c33c3d6d40ff1cbc688e
Loveorigami <loveorigami@mail.ru>
2017-01-17 13:19:33 +0300

    bootstrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,74 @@
+<?php
+namespace lo\plugins;
+
+use lo\plugins\components\PluginsManager;
+use lo\plugins\components\View;
+use lo\plugins\core\ShortcodeHandler;
+use lo\plugins\repositories\EventDbRepository;
+use lo\plugins\services\ShortcodeService;
+use lo\plugins\shortcodes\ShortcodeParser;
+use Yii;
+use yii\base\BootstrapInterface;
+use yii\base\Event;
+
+/**
+ * Class Bootstrap
+ * @package lo\plugins\components
+ * @author Lukyanov Andrey <loveorigami@mail.ru>
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * @param \yii\base\Application $app
+     */
+    public function bootstrap($app)
+    {
+        if (!isset(Yii::$app->i18n->translations['plugin'])) {
+            Yii::$app->i18n->translations['plugin'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en',
+                'basePath' => '@lo/plugins/messages'
+            ];
+        }
+
+        /** @var PluginsManager $pluginManager */
+        $pluginManager = $app->plugins;
+        $appId = $pluginManager->appId;
+
+        if ($pluginManager->enablePlugins && $appId) {
+            $this->registerEvents($appId);
+        }
+
+        if ($pluginManager->shortcodesParse) {
+            Yii::$container->setSingleton(ShortcodeParser::class);
+            Yii::$container->set(ShortcodeService::class);
+            Event::on(View::class, View::EVENT_DO_BODY, [
+                ShortcodeHandler::class, ShortcodeHandler::PARSE_SHORTCODES
+            ], $pluginManager);
+        }
+    }
+
+    /**
+     * @param $appId
+     */
+    protected function registerEvents($appId)
+    {
+        $repository = new EventDbRepository();
+        /** @var  \lo\plugins\models\Event [] $events */
+        $events = $repository->findEventsByApp($appId);
+        if ($events) {
+            foreach ($events as $event) {
+                $triggerClass = $event->getTriggerEvent();
+                $triggerEvent = $event->getTriggerEvent();
+                $handler = $event->getHandler();
+                if (is_array($handler) && is_callable($handler[0])) {
+                    $data = isset($handler[1]) ? array_pop($handler) : null;
+                    $append = isset($handler[2]) ? array_pop($handler) : null;
+                    Event::on($triggerClass, $triggerEvent, $handler[0], $data, $append);
+                } else if (is_callable($handler)) {
+                    Event::on($triggerClass, $triggerEvent, $handler);
+                }
+            }
+        }
+    }
+}
