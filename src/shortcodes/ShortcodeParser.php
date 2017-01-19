cee51ee064a4718a320de4cde9cb8178b679794d
Loveorigami <loveorigami@mail.ru>
2017-01-19 11:34:58 +0300

    shortcodes helper

--- src/shortcodes/ShortcodeParser.php
+++ src/shortcodes/ShortcodeParser.php
@@ -1,6 +1,7 @@
 <?php
 namespace lo\plugins\shortcodes;
 
+use lo\plugins\helpers\ShortcodesHelper;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -52,18 +53,6 @@ class ShortcodeParser
      * @var string
      */
     private $_ignorePattern;
-
-    /**
-     * The regex for attributes.
-     * This regex covers the following attribute situations:
-     *  - key = "value"
-     *  - key = 'value'
-     *  - key = value
-     *  - "value"
-     *  - value
-     * @var string
-     */
-    private $attrPattern = '/(\w+)\s*=\s*"([^"]*)"(?:\s|$)|(\w+)\s*=\s*\'([^\']*)\'(?:\s|$)|(\w+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
 
     /**
      * Associative array of shortcodes and their
@@ -292,7 +281,7 @@ class ShortcodeParser
         if (empty($this->_shortcodes)) {
             return $content;
         }
-        return preg_replace_callback($this->shortcodeRegex(), array($this, 'stripShortcodeTag'), $content);
+        return preg_replace_callback($this->shortcodeRegex(), [$this, 'stripShortcodeTag'], $content);
     }
 
     /**
@@ -367,84 +356,20 @@ class ShortcodeParser
 
     /**
      * Parses attributes from a shortcode
-     * Borrowed from WordPress wp/wp-includes/shortcode.php
      * @param string $text
      * @return array
      */
     protected function parseAttributes($text)
     {
-        $text = preg_replace("/[\x{00a0}\x{200b}]+/u", " ", $text);
-        if (!preg_match_all($this->attrPattern, $text, $matches, PREG_SET_ORDER)) {
-            return array(ltrim($text));
-        }
-        $attr = array();
-        foreach ($matches as $match) {
-            if (!empty($match[1])) {
-                $attr[strtolower($match[1])] = stripcslashes($match[2]);
-            } elseif (!empty($match[3])) {
-                $attr[strtolower($match[3])] = stripcslashes($match[4]);
-            } elseif (!empty($match[5])) {
-                $attr[strtolower($match[5])] = stripcslashes($match[6]);
-            } elseif (isset($match[7]) && strlen($match[7])) {
-                $attr[] = stripcslashes($match[7]);
-            } elseif (isset($match[8])) {
-                $attr[] = stripcslashes($match[8]);
-            }
-        }
-        return (array)$attr;
+        return ShortcodesHelper::parseAttributes($text);
     }
 
     /**
-     * Retrieve the shortcode regular expression for searching.
-     *
-     * The regular expression combines the shortcode tags in the regular expression
-     * in a regex class.
-     *
-     * The regular expression contains 6 different sub matches to help with parsing.
-     *
-     * 1 - An extra [ to allow for escaping shortcodes with double [[]]
-     * 2 - The shortcode name
-     * 3 - The shortcode argument list
-     * 4 - The self closing /
-     * 5 - The content of a shortcode when it wraps some content.
-     * 6 - An extra ] to allow for escaping shortcodes with double [[]]
-     *
      * @return string The shortcode search regular expression
      */
     protected function shortcodeRegex()
     {
-        $tagRegex = join('|', array_map('preg_quote', array_keys($this->_shortcodes)));
-        return
-            '/'
-            . '\\['                              // Opening bracket
-            . '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
-            . "($tagRegex)"                      // 2: Shortcode name
-            . '(?![\\w-])'                       // Not followed by word character or hyphen
-            . '('                                // 3: Unroll the loop: Inside the opening shortcode tag
-            . '[^\\]\\/]*'                   // Not a closing bracket or forward slash
-            . '(?:'
-            . '\\/(?!\\])'               // A forward slash not followed by a closing bracket
-            . '[^\\]\\/]*'               // Not a closing bracket or forward slash
-            . ')*?'
-            . ')'
-            . '(?:'
-            . '(\\/)'                        // 4: Self closing tag ...
-            . '\\]'                          // ... and closing bracket
-            . '|'
-            . '\\]'                          // Closing bracket
-            . '(?:'
-            . '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
-            . '[^\\[]*+'             // Not an opening bracket
-            . '(?:'
-            . '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
-            . '[^\\[]*+'         // Not an opening bracket
-            . ')*+'
-            . ')'
-            . '\\[\\/\\2\\]'             // Closing shortcode tag
-            . ')?'
-            . ')'
-            . '(\\]?)'                           // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
-            . '/s';
+        return ShortcodesHelper::shortcodeRegex($this->_shortcodes);
     }
 }
 
